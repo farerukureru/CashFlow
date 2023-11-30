19bd073e7249df03f5683c3d2ccb6fe7b8e58a4c
Arfan301 <maulana.arfan301@gmail.com>
2023-12-01 03:47:58 +0700

    added the backend for bottom navigation view and making the home UI, also added profile fragment

--- app/src/main/java/com/example/cashflow/MainActivity.java
+++ app/src/main/java/com/example/cashflow/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.cashflow;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -16,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch(item.getItemId()){
-//                case R.id.homeNavbar:
-//                    break;
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            }else if (item.getItemId() == R.id.profile) {
+                replaceFragment(new ProfileFragment());
             }
             return true;
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frame_id,fragment);
+        ft.commit();
     }
 }
